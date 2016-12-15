103477fbb3629a30464c3c65273d1b81d1ca33db
Philip Domingo <philipdomingo@outlook.com>
2016-12-15 17:51:50 +0800

    Added audio for Bulbasaur

---
+++ Assets/Scripts/PokemonInteraction.cs
@@ -0,0 +1,13 @@
+﻿using HoloToolkit.Unity.InputModule;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class PokemonInteraction : MonoBehaviour, IInputClickHandler
+{
+    public void OnInputClicked(InputEventData eventData)
+    {
+        gameObject.GetComponent<AudioSource>().Play();
+    }
+}
